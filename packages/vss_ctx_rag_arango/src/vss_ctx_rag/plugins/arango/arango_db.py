aa8756234d90ce5ec251abca80c9c76b6b889c79
David Du <dadu@nvidia.com>
2026-05-15 14:35:00 -0700

    mark false positives
    Signed-off-by: David Du <dadu@nvidia.com>

--- packages/vss_ctx_rag_arango/src/vss_ctx_rag/plugins/arango/arango_db.py
+++ packages/vss_ctx_rag_arango/src/vss_ctx_rag/plugins/arango/arango_db.py
@@ -1307,7 +1307,7 @@ class ArangoGraphDB(NetworkXGraphDB):
 
     def fetch_subtitle_for_embedding(self) -> List[Dict[str, Any]]:
         """Fetch subtitle nodes without embeddings for embedding processing."""
-        with Metrics("GraphRAG/ArangoDB/fetch_subtitle_for_embedding", "green"):
+        with Metrics("GraphRAG/ArangoDB/fetch_subtitle_for_embedding", "green"):  # pragma: allowlist secret
             query = """
                 LET subtitles = (
                     FOR s IN @@collection
